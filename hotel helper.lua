851e50f833d5852d235df04d36881658ee392e6b
Serhiy <48212972+LirysJH@users.noreply.github.com>
2019-08-21 20:04:17 +0300

    Update hotel helper.lua

--- hotel helper.lua
+++ hotel helper.lua
@@ -2,13 +2,13 @@ script_name("Hotel Helper")
 script_author("James Hawk")
 script_version("001")
 
+local sampev = require "lib.samp.events"
 local state = true
 
 local data = {
 	av_price = 0,
 	time = 0,
-	sum = 0,
-	id = 343
+	sum = 0
 }
 
 function main()
@@ -24,4 +24,21 @@ function main()
 	end)
 	
 	wait(-1)
+end
+
+function sampev.onShowDialog(id, style, title, button1, button2, text)
+	lua_thread.create(function()
+		if id == 343 and state then
+			sampSendDialogResponse(id, 1, 1, -1)
+			wait(50)
+			data.av_price, data.time = string.match(text,'[^%d+]+(%d+)[^%d+]+[%d+]+[^%d+]+[%d+]+[^%d+]+(%d+)[^%d+]')
+			data.time = 100 - tonumber(data.time)
+			data.sum = data.time*tonumber(data.av_price)
+			if data.sum > 0 then
+				sampSendDialogResponse(id, 1, -1, data.sum)
+			end
+			wait(20)
+			sampCloseCurrentDialogWithButton(0)
+		end
+	end)
 end
